e106975d2e52cf5a2ef7f287c848cc9c010b8a0d
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2024-11-15 21:23:41 +0100

    feat: Error Handling with anyhow

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,32 @@
 use std::{fs, path::PathBuf};
 
-fn extract_json_metadata(file: &PathBuf) -> serde_json::Value {
+use anyhow::{Context, Ok, Result, Error};
+
+fn extract_json_metadata(file: &PathBuf) -> Result<serde_json::Value> {
     // Parse the Matroska file
-    let matroska = matroska::open(file).unwrap();
+    let matroska = matroska::open(file)
+        .context(format!("Unable to parse Matroska file: {:?}", file))?;
     // Find the json attachment
-    let json_attachment = matroska.attachments.into_iter().find(|x| x.name.eq("info.json")).unwrap();
+    let json_attachment = matroska.attachments.into_iter()
+        .find(|x| x.name.ends_with(".json"))
+        .ok_or(Error::msg(format!("Unable to find JSON attachment in file: {:?}", file)))?;
     // Parse it as JSON and return the result
-    serde_json::from_slice(&json_attachment.data).unwrap()
+    serde_json::from_slice(&json_attachment.data).map_err(anyhow::Error::from)
+        .context(format!("Unable to parse JSON for file: {:?}", file))
 }
 
 // Renames "file" to "new_name" without changing it's directory 
-fn rename_file(file: &PathBuf, new_name: &str) {
+fn rename_file(file: &PathBuf, new_name: &str) -> Result<()> {
     let mut new_path = file.clone();
     new_path.set_file_name(new_name);
-    fs::rename(file, new_path).unwrap()
+    fs::rename(file, new_path)
+        .map_err(anyhow::Error::from)
+        .context(format!("Unable to rename file {:?}", file))
 }
 
-pub fn rename_video(file: PathBuf) {
+pub fn rename_video(file: PathBuf) -> Result<()> {
     // Extract JSON metadata from video-file
-    let json: serde_json::Value = extract_json_metadata(&file);
+    let json: serde_json::Value = extract_json_metadata(&file)?;
 
     // Construct new filename
     let new_filename = format!(
@@ -28,19 +36,20 @@ pub fn rename_video(file: PathBuf) {
     );
 
     // Finally, rename the file to the right filename.
-    rename_file(&file, &new_filename);
+    rename_file(&file, &new_filename)
 }
 
-pub fn rename_videos(in_dir: PathBuf) {
+pub fn rename_videos(in_dir: PathBuf) -> Result<()> {
     for entry in
-        // Iterator over all Entries in directory
-        fs::read_dir(in_dir).unwrap().flatten()
+        // Iterator over all (valid) Entries in directory
+        fs::read_dir(in_dir)?.flatten()
         // Filter for mkv files
         .filter(|x| x.file_name().into_string().unwrap().ends_with(".mkv"))
         // Unwrap the paths
         .map(|x|x.path())
     {
-        rename_video(entry)
+        rename_video(entry)?
     }
+    Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use std::path::PathBuf;
 
+use anyhow::Result;
 use ytdl_json_renamer::*;
 
-fn main() {
-    let dir = PathBuf::from("./test-files");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Location of file/files to rename
+    path: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let dir = PathBuf::from(args.path);
+
     rename_videos(dir)
 }
