2fc9e2ec7e5b516a9e4ab2804288d8a2cd79f48e
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2024-11-15 18:56:26 +0100

    refactor: Split logic into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,8 @@
-use std::fs;
+use std::{fs, path::{Path, PathBuf}};
+
+use ytdl_json_renamer::*;
 
 fn main() {
-    let dir = "./test-files";
-    let paths = fs::read_dir(dir).unwrap();
-    for path in paths.map(|p| p.unwrap()) {
-        if path.file_name().into_string().unwrap().ends_with(".mkv") {
-            println!("Found MKV-file: {}", path.file_name().into_string().unwrap());
-            let matroska = matroska::open(path.path()).unwrap();
-            let json_attachment = matroska.attachments.into_iter().find(|x| x.name.eq("info.json")).unwrap();
-            let json: serde_json::Value = serde_json::from_slice(&json_attachment.data).unwrap();
-            
-            let new_filename = format!(
-                "{} [{}].mkv",
-                json["title"].as_str().unwrap(),
-                json["id"].as_str().unwrap()
-            );
-
-            let mut new_path = path.path().clone();
-            new_path.set_file_name(new_filename);
-            println!("Renaming {} to {}", path.path().file_name().unwrap().to_str().unwrap(), new_path.file_name().unwrap().to_str().unwrap());
-            fs::rename(path.path(), new_path).unwrap();
-
-        } else {
-            println!("BAD FILE: {:?}", path)
-        }
-    }
+    let dir = PathBuf::from("./test-files");
+    recursive_rename(dir)
 }
