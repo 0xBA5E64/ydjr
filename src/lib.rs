83344fd697b6ec3bf14057d38691ac9dcdb86005
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2024-11-15 21:24:11 +0100

    fix: cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,27 @@
 use std::{fs, path::PathBuf};
 
-use anyhow::{Context, Ok, Result, Error};
+use anyhow::{Context, Error, Ok, Result};
 
 fn extract_json_metadata(file: &PathBuf) -> Result<serde_json::Value> {
     // Parse the Matroska file
-    let matroska = matroska::open(file)
-        .context(format!("Unable to parse Matroska file: {:?}", file))?;
+    let matroska =
+        matroska::open(file).context(format!("Unable to parse Matroska file: {:?}", file))?;
     // Find the json attachment
-    let json_attachment = matroska.attachments.into_iter()
+    let json_attachment = matroska
+        .attachments
+        .into_iter()
         .find(|x| x.name.ends_with(".json"))
-        .ok_or(Error::msg(format!("Unable to find JSON attachment in file: {:?}", file)))?;
+        .ok_or(Error::msg(format!(
+            "Unable to find JSON attachment in file: {:?}",
+            file
+        )))?;
     // Parse it as JSON and return the result
-    serde_json::from_slice(&json_attachment.data).map_err(anyhow::Error::from)
+    serde_json::from_slice(&json_attachment.data)
+        .map_err(anyhow::Error::from)
         .context(format!("Unable to parse JSON for file: {:?}", file))
 }
 
-// Renames "file" to "new_name" without changing it's directory 
+// Renames "file" to "new_name" without changing it's directory
 fn rename_file(file: &PathBuf, new_name: &str) -> Result<()> {
     let mut new_path = file.clone();
     new_path.set_file_name(new_name);
@@ -42,14 +48,14 @@ pub fn rename_video(file: PathBuf) -> Result<()> {
 pub fn rename_videos(in_dir: PathBuf) -> Result<()> {
     for entry in
         // Iterator over all (valid) Entries in directory
-        fs::read_dir(in_dir)?.flatten()
-        // Filter for mkv files
-        .filter(|x| x.file_name().into_string().unwrap().ends_with(".mkv"))
-        // Unwrap the paths
-        .map(|x|x.path())
+        fs::read_dir(in_dir)?
+            .flatten()
+            // Filter for mkv files
+            .filter(|x| x.file_name().into_string().unwrap().ends_with(".mkv"))
+            // Unwrap the paths
+            .map(|x| x.path())
     {
         rename_video(entry)?
     }
     Ok(())
 }
-
