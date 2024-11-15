fac9ec186634a451a50fc5e732776a7cf1862294
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2024-11-15 19:39:11 +0100

    refactor: Add documentation to functions and restructure `rename_videos` iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,46 @@
 use std::{fs, path::PathBuf};
 
 fn extract_json_metadata(file: &PathBuf) -> serde_json::Value {
+    // Parse the Matroska file
     let matroska = matroska::open(file).unwrap();
+    // Find the json attachment
     let json_attachment = matroska.attachments.into_iter().find(|x| x.name.eq("info.json")).unwrap();
+    // Parse it as JSON and return the result
     serde_json::from_slice(&json_attachment.data).unwrap()
 }
 
+// Renames "file" to "new_name" without changing it's directory 
 fn rename_file(file: &PathBuf, new_name: &str) {
     let mut new_path = file.clone();
     new_path.set_file_name(new_name);
-    //println!("Renaming {} to {}", entry.path().file_name().unwrap().to_str().unwrap(), new_path.file_name().unwrap().to_str().unwrap());
     fs::rename(file, new_path).unwrap()
 }
 
-pub fn rename_video(file: &PathBuf) {
-    let json: serde_json::Value = extract_json_metadata(file);
+pub fn rename_video(file: PathBuf) {
+    // Extract JSON metadata from video-file
+    let json: serde_json::Value = extract_json_metadata(&file);
 
+    // Construct new filename
     let new_filename = format!(
         "{} [{}].mkv",
         json["title"].as_str().unwrap(),
         json["id"].as_str().unwrap()
     );
 
+    // Finally, rename the file to the right filename.
     rename_file(&file, &new_filename);
 }
 
 pub fn rename_videos(in_dir: PathBuf) {
-    let paths = fs::read_dir(in_dir).unwrap();
-    for entry in paths
-        .filter_map(|x| Some(x.unwrap()) )
-        .filter(|p| p.file_name().into_string().unwrap().ends_with(".mkv") )
+    for entry in
+        // Iterator over all Entries in directory
+        fs::read_dir(in_dir).unwrap().flatten()
+        // Filter for mkv files
+        .filter(|x| x.file_name().into_string().unwrap().ends_with(".mkv"))
+        // Unwrap the paths
+        .map(|x|x.path())
     {
-        rename_video(&entry.path())
+        rename_video(entry)
     }
-
 }
 
